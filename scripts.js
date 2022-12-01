d18b5061204a7fddf604812b100cbcf1c8bcefb8
Motti789 <mchillywilly@gmail.com>
2022-12-01 12:54:13 -0500

    extracted the drink name ingredients and id

--- scripts.js
+++ scripts.js
@@ -2,35 +2,27 @@
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
 
 
-// fetch(url)
-// .then(resp => resp.json())
-// .then(data => {
-//     for(let dat in data) {
-//      console.log(data[dat])
-//     }
-// })
-
-
-
-
-
-
-
-// const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
-
-
 fetch(url)
 .then(resp => resp.json())
 .then(data => {
     for(let drinks in data) {
         for (let drink in drinks){
-        let drinkName = data[drinks][drink]["strDrink"];
-        let drinkId = data[drinks][drink]["idDrink"];
-        console.log(drinkName)
+        let drinkData = data[drinks][drink]
+        let drinkName = drinkData["strDrink"];
+        let drinkId =   drinkData["idDrink"];
+        let instructions = drinkData["strInstructions"];
+        let ingredients1 = drinkData["strIngredient1"]
+        let ingredients2 = drinkData["strIngredient2"]
+        console.log(drinkName);
+        
+        console.log(ingredients1)
+        console.log(ingredients2)
+        console.log("")
+        // console.log(instructions)
         }
-  
     }
 })
+
 
 
 
