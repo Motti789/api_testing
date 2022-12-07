607edbba4631e96ff01740d3a63c8cf4afd78875
Motti789 <mchillywilly@gmail.com>
2022-12-07 13:04:14 -0500

    deleted the array of imags and pulled it from the api instead

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
 
 const app = document.getElementById("root")
@@ -25,6 +26,9 @@ fetch(url)
         let drinkName = drinkData["strDrink"];
         let drinkId =   drinkData["idDrink"];
         let instructions = drinkData["strInstructions"];
+        let image = drinkData["strDrinkThumb"];
+
+        
 
         let ingredients = [drinkData["strIngredient1"], drinkData["strIngredient2"], drinkData["strIngredient3"], 
         drinkData["strIngredient4"], drinkData["strIngredient5"], drinkData["strIngredient6"], 
@@ -40,6 +44,14 @@ fetch(url)
         const p1 = document.createElement('p')
         p1.textContent = instructions
 
+        const img = document.createElement('img')
+        const card_front = document.createElement('div')
+        card_front.setAttribute('class', "card__face card__face--front")
+        img.src = image
+        img.width="350"
+        card_front.appendChild(img)
+        
+        card.appendChild(card_front)
         card.appendChild(card_back)
         card_back.appendChild(h1)
         card_back.appendChild(p1)
@@ -49,7 +61,7 @@ fetch(url)
          
             const p2 = document.createElement('p')
             p2.textContent = ingredient
-            card_back.appendChild(p2)
+            // card_back.appendChild(p2)
         }
        }
         }
@@ -59,31 +71,6 @@ fetch(url)
 
 
 
-// const logo = document.createElement('img')
-// logo.src =  "https://www.thecocktaildb.com/images/media/drink/tqyrpw1439905311.jpg"
-// app.appendChild(logo)
-
-
-
-const images = [
-"https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg",
-"https://www.thecocktaildb.com/images/media/drink/bry4qh1582751040.jpg",
-"https://www.thecocktaildb.com/images/media/drink/loezxn1504373874.jpg",
-"https://www.thecocktaildb.com/images/media/drink/srpxxp1441209622.jpg",
-"https://www.thecocktaildb.com/images/media/drink/tqyrpw1439905311.jpg",
-"https://www.thecocktaildb.com/images/media/drink/dztcv51598717861.jpg",
-]
-
-for(let image of images) {
-  const img = document.createElement('img')
-  const card_front = document.createElement('div')
-  card_front.setAttribute('class', "card__face card__face--front")
-  img.src = image
-  img.width="350"
-  card_front.appendChild(img)
-  card.appendChild(card_front)
-
-}
 
 card.addEventListener('click', flipCard)
 
